b7b233f32cecf33b2bbac1b3bae3f93036b57ffa
Chad Smith <smith.chad12@gmail.com>
2021-09-16 22:16:29 -0500

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Chad Smith on 9/16/21.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
